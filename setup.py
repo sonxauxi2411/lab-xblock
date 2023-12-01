94fb63feab94c98b9c935d458bd072edf53f9aa3
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2023-12-01 11:17:03 +0700

    chore : update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def package_data(pkg, roots):
 
 setup(
     name='labxblock-xblock',
-    version='0.2',
+    version='0.3',
     description='labxblock XBlock',   
     license='UNKNOWN',          
     packages=[
