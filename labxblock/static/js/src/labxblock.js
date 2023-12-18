7303d37625660e8cf4fb43074ff626f63c02cea6
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2023-12-18 18:03:55 +0700

    feat: update version 0.3.7

--- labxblock/static/js/src/labxblock.js
+++ labxblock/static/js/src/labxblock.js
@@ -103,7 +103,7 @@ function LabXBlock(runtime, element) {
                         </div>
                         <div>
                         <input type="file" name="file" id="file" class="inputfile"  />      
-                        <label for="file">${trans('Select file')}</label>
+                        <label class='btn-primary-custom ' for="file"><span>${trans('Select file')}</span></label>
                         </div>
                             <div id='file-info'></div>
                         `;
@@ -192,9 +192,11 @@ function LabXBlock(runtime, element) {
         let year = dateObject.getFullYear();
         let month = dateObject.getMonth() + 1; 
         let day = dateObject.getDate();
-        let formattedDate = day + '/' + month + '/' + year;
+        let formattedDate = day + ' tháng ' + month + ', ' + year;
         let text = `${trans('Submission date')}: ${formattedDate}`
         $('.lab-date-success', element).html(text)
+        $('.lab-date-success', element).addClass('lab-date ')
+
     }
 
     function viewResultText(data){
@@ -206,7 +208,12 @@ function LabXBlock(runtime, element) {
                 <div class='student-lab'>
                     <div class='student-lab-title'>
                         <span>${trans('Lab content')}</span>
-                        <span  id='student-lab-edit'><i class="fa fa-pencil-square-o" aria-hidden="true"></i></span>
+                        <span  id='student-lab-edit'>
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.8924 0.165122C13.1554 0.0561096 13.4373 0 13.722 0C14.0067 0 14.2886 0.0561096 14.5516 0.165122C14.8145 0.27409 15.0533 0.433781 15.2545 0.635071L15.8647 1.24535C15.8647 1.24533 15.8648 1.24537 15.8647 1.24535C16.0659 1.44652 16.2256 1.68541 16.3345 1.94826C16.4434 2.21114 16.4995 2.4929 16.4995 2.77745C16.4995 3.06199 16.4434 3.34375 16.3345 3.60663C16.2256 3.86943 16.0661 4.10822 15.8649 4.30936C15.8649 4.30942 15.865 4.3093 15.8649 4.30936L9.68843 10.4891C9.28185 10.8957 8.7302 11.1241 8.15586 11.1241H6.06072C5.68201 11.1241 5.375 10.8171 5.375 10.4383V8.35621C5.37503 7.78201 5.60293 7.23125 6.00866 6.82493L12.1892 0.635421C12.3904 0.434016 12.6295 0.274134 12.8924 0.165122ZM13.722 1.37145C13.6176 1.37145 13.5141 1.39204 13.4176 1.43204C13.3211 1.47203 13.2335 1.53064 13.1597 1.60451C13.1596 1.60454 13.1597 1.60449 13.1597 1.60451L6.97913 7.79398C6.97913 7.79398 6.97913 7.79398 6.97913 7.79398C6.83016 7.94317 6.74647 8.14538 6.74645 8.35621C6.74645 8.35619 6.74645 8.35622 6.74645 8.35621V9.75261H8.15586C8.36652 9.75261 8.56912 9.66883 8.71842 9.5196C8.71837 9.51965 8.71846 9.51956 8.71842 9.5196L14.8949 3.33985C14.9688 3.26603 15.0275 3.17819 15.0675 3.08173C15.1074 2.98526 15.128 2.88186 15.128 2.77745C15.128 2.67303 15.1074 2.56964 15.0675 2.47317C15.0275 2.3767 14.9689 2.28905 14.8951 2.21522L14.2847 1.60483C14.2109 1.53092 14.123 1.47205 14.0264 1.43204C13.9299 1.39204 13.8265 1.37145 13.722 1.37145Z" fill="#2C3744"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M4.57867 1.49923C3.65871 1.6229 3.12864 1.85484 2.74174 2.24174C2.35481 2.62867 2.12289 3.15857 1.99923 4.07838C1.87291 5.01792 1.87145 6.25648 1.87145 8C1.87145 9.74311 1.87291 10.9817 1.99923 11.9213C2.1229 12.8413 2.35484 13.3714 2.74174 13.7583C3.12867 14.1452 3.65857 14.3771 4.57838 14.5008C5.51792 14.6271 6.75648 14.6286 8.5 14.6286C10.2431 14.6286 11.4817 14.6271 12.4213 14.5008C13.3413 14.3771 13.8714 14.1452 14.2583 13.7583C14.6452 13.3713 14.8771 12.8414 15.0008 11.9216C15.1271 10.9821 15.1286 9.74352 15.1286 8C15.1286 7.62128 15.4356 7.31428 15.8143 7.31428C16.193 7.31428 16.5 7.62128 16.5 8V8.05158C16.5 9.73196 16.5 11.0628 16.36 12.1044C16.2159 13.1763 15.9122 14.0438 15.228 14.728C14.5438 15.4123 13.676 15.7159 12.6041 15.86C11.5624 16 10.2316 16 8.55157 16H8.44842C6.76804 16 5.43717 16 4.39563 15.86C3.32372 15.7159 2.45618 15.4122 1.77198 14.728C1.08774 14.0438 0.784115 13.176 0.640006 12.1041C0.499974 11.0624 0.499986 9.73155 0.5 8.05156V7.94841C0.499986 6.26804 0.499974 4.93717 0.640007 3.89563C0.784125 2.82372 1.08778 1.95618 1.77198 1.27198C2.45621 0.587745 3.32399 0.284115 4.39594 0.140006C5.43758 -2.56951e-05 6.76845 -1.4202e-05 8.44844 2.94042e-07L8.5 6.21021e-07C8.87872 6.21021e-07 9.18573 0.30701 9.18573 0.685725C9.18573 1.06444 8.87872 1.37145 8.5 1.37145C6.75689 1.37145 5.51832 1.37291 4.57867 1.49923Z" fill="#2C3744"/>
+                            </svg>
+                        </span>
                     </div>
                     <div  class='student-lab-content'>
                         <span id='content-student-lab'> ${data.result_student}</span>
@@ -242,6 +249,8 @@ function LabXBlock(runtime, element) {
         });
 
         $("#student-lab-edit" , element).click(function(){
+            console.log('=========', $('.student-lab-content' , element))
+            $('.student-lab-content' , element).addClass('bg-none')
             $("#content-student-lab" , element).addClass('none')
             const labElement = $(element).find('.student-lab-content');
             let newDiv = `
@@ -252,12 +261,12 @@ function LabXBlock(runtime, element) {
                 <div class='btn-edit'>  
                         
                         <div class="lab-btn ">
-                            <span id='lab-huy'  class="lab-button" >
+                            <span id='lab-huy'  class="btn-primary-custom-outline lab-button" >
                                 <span class="text-lab">Huỷ</span>
                             </span>
                         </div>
                         <div class="lab-btn ">
-                            <span id='student-edit-submit'  class="lab-button" >
+                            <span id='student-edit-submit'  class="btn-primary-custom lab-button" >
                                 <span class="text-lab">Chỉnh sửa</span>
                             </span>
                         </div>
@@ -268,10 +277,12 @@ function LabXBlock(runtime, element) {
              ClassicEditor
             .create( element.querySelector( '#editor' ) ).then(editor =>{
                 $('#lab-huy', element).click(function(){
+                    $('.student-lab-content' , element).removeClass('bg-none')
                     $("#content-student-lab" , element).removeClass('none')
                     $("#lab-edit" , element).remove()
                 })
                 $('#student-edit-submit' , element).click(function(){
+                    $('.student-lab-content' , element).removeClass('bg-none')
                     const editorData = editor.getData();
                     const formData = new FormData();
                     formData.append("text", editorData);
@@ -302,8 +313,8 @@ function LabXBlock(runtime, element) {
         const text = `
         <div>
             <div class='student-lab'>
-                <div >
-                    <a class='student-file'  href=${data.url} target='_blank' > 
+                <div style='padding:10px 0px;' >
+                    <a class='btn-primary-custom-outline student-file'  href=${data.url} target='_blank' > 
                         <span>${trans('Download submitted lab')} </span>
                     </a>
                 </div>
@@ -314,10 +325,11 @@ function LabXBlock(runtime, element) {
                 <span> ${trans('View Lab reference answers')}</span>
                 <details class="detail-lab-result">
                     <summary class='result-summary'>
-                        <span>${trans('Reference answers')}</span>
-                        <span style='color:black'><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                       <span>${trans('Reference answers')}</span>
+                       <span style='color:black'><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+                     
                     </summary>
-                    <div >
+                    <div style='padding-bottom:6px'>
                         <span style='padding-bottom:10px; padding-top:5px'>${data.result}</span>
                     </div>
                 </details>
@@ -328,6 +340,15 @@ function LabXBlock(runtime, element) {
     `
     $(".lab-content", element).html(text);
     $('#lab-notification').removeClass('none');
+
+    $(element).find('.detail-lab-result').on('toggle', function() {
+        var $iconSpan = $(element).find('.fa-chevron-right');
+        if ($(this).prop('open')) {
+            $iconSpan.hide(); 
+        } else {
+            $iconSpan.show(); 
+        }
+    })
     }
 
 
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def package_data(pkg, roots):
 
 setup(
     name='labxblock-xblock',
-    version='0.3.6',
+    version='0.3.7',
     description='labxblock XBlock',   
     license='UNKNOWN',          
     packages=[
