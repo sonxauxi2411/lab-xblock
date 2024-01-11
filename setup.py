ed86ea243f23829efd672d0b7f771de928d1d239
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2024-01-11 14:05:05 +0700

    fix : css , update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def package_data(pkg, roots):
 
 setup(
     name='labxblock-xblock',
-    version='0.4.8',
+    version='0.4.9',
     description='labxblock XBlock',   
     license='UNKNOWN',          
     packages=[
