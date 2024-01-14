c7f30e6c67cfa796a47e37928cc26c4feeaff9ec
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2024-01-14 20:30:44 +0700

    chore : min-heighht lab xblock

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def package_data(pkg, roots):
 
 setup(
     name='labxblock-xblock',
-    version='0.4.10',
+    version='0.4.12',
     description='labxblock XBlock',   
     license='UNKNOWN',          
     packages=[
